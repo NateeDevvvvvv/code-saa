537a9b37777bbedaa33c81c82c42fff6d63622e4
NateeDevvvvvv <sleepbns1237@gmail.com>
2025-12-14 19:09:39 -0600

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,4 @@
--- NateeHubbb Full Script
+
 local OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Orion/main/source'))()
 
 local Window = OrionLib:MakeWindow({Name = "NateeHubbb", HidePremium = false, SaveConfig = true, ConfigFolder = "NateeHubbb"})
